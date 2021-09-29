33da99ab16552f1867e27ff658e84051a21a1c88
it21067488 <88025761+it21067488@users.noreply.github.com>
2021-09-29 01:17:11 +0000

    Converted C programs to C++ programs.

--- tute04.cpp
+++ tute04.cpp
@@ -10,20 +10,48 @@ Implement the functions
 long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
-
+/*
 #include <iostream>
+using namespace std;
 
 long Factorial(int no);
 long nCr(int n, int r);
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
+  cout << "Enter a value for n ";
+  cin >> n;
+  cout << "Enter a value for r ";
+  cin >> r;
+  cout << "nCr = ";
+  cout << nCr(n,r);
+  cout << std::endl;
   return 0;
+}*/
+#include <iostream>
+using namespace std;
+ 
+int fact(int);
+ 
+int main(void)
+{
+ int n,r,ncr;
+ 
+  	cout<<"Enter a number n : ";
+  	cin>>n;
+ cout<<"Enter a number r : ";
+  	cin>>r;
+
+  	ncr=fact(n)/(fact(r) * fact(n-r));
+    cout<<"Value of "<<n<<r<<"="<<ncr;
+}
+ 
+int fact(int n)
+{
+    int i,f=1;
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    return f;
 }
